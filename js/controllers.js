ca2f7728bcfc332cf4ea23cbd05f529848257784
TheRealest <ryanprov@mac.com>
2012-09-30 23:07:27 -0700

    cleaning up first Wildfire implementation

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,5 @@
 function WelcomeCtrl($scope,wildfire) {
-	
+	$scope.test = 'tree';
 	$scope.test = wildfire.getValue($scope,'test');
 
 }
--- js/services.js
+++ js/services.js
@@ -1,14 +1,15 @@
 function Wildfire($http,$q) {
-	// FIREBASE VARIABLE PREFIX CONVENTION:
-	// rl: relative link
-	// fb: Firebase object
-	// ss: snapshot
-	// ob: Javascript object literal
+	// Firebase variable prefix convention:
+	// ------------------------------------
+	// * `rl`: relative link
+	// * `fb`: Firebase object
+	// * `ss`: Firebase snapshot
+	// * `ob`: Javascript object literal
 
 	var fbRoot = new Firebase('http://gamma.firebase.com/therealest');
 
 	function getValue(scope,rl) {
-		// Create a deferref object with $q
+		// Create a deferred object with `$q`
 		var deferred = $q.defer();
 		var ob;
 
@@ -18,17 +19,25 @@ function Wildfire($http,$q) {
 			// Get the data stored at this Firebase location
 			ob = obFROMss(ss);
 
-			// We need to wrap the deferred resolution in an $apply call so that the view properly updates when the asynchronous call is completed
-			// We pass the scope of the controller to the Wildfire function so that the controller can remain ignorant to promises, deferreds, and $q
+			// We need to wrap the deferred resolution in an `$apply` call so that the view properly updates when the asynchronous call is completed
+
+			// We pass the scope of the controller to the Wildfire function so that the controller can remain ignorant to promises, deferreds, and `$q`
 			scope.$apply(function() {
-				// This tells deferred to set its promise equal to ob when the asynchronous call for ob is completed
+				// This tells deferred to set its promise equal to `ob` when the asynchronous call for `ob` is completed
 				deferred.resolve(ob);
 			});
 		});
 
 		// Finally we return a promise so that the controller which called this function knows to expect the result when the asynchronous call is complete
+
 		// In the mean time the view will simply be empty
 		return deferred.promise;
+	}
+
+	function updateValue(variable,rl) {
+		fbFROMrl(rl).on('value', function(ss) {
+			
+		});
 	}
 
 	// Firebase conversion helper functions
